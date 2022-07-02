549ab35d4a4997138149831a435138eb5aa8e542
Belo_cg86@itstepru.onmicrosoft.com <Belo_cg86@itstepru.onmicrosoft.com>
2022-07-02 14:33:28 +0500

    pattrn

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -4,32 +4,71 @@ using namespace std;
 class ISubscribe {
 public:
 	virtual void onEmit() = 0;
-
+};
 class Observer {
 public:
 	virtual void subscribe(ISubscribe& subscriber) = 0;
 	virtual void publish(string payload)=0;
+private:
 	list<ISubscribe>list;
 };
-class ModelObserver:public Observer {
+class Controller:public ISubscribe {
+public:
+	void operation(){}
+
+	//Model model;
+};
+class Model :public Controller {
+public:
+	void get_data()const{}
+	string set_data(){}
+	string data;
+	//ModelObserver observer;
+};
+class ModelObserver:public Observer,public Model {
 public:
 	void subscribe(ISubscribe& subscriber) {}
 	void publish(string payload) {}
+private:
+	list<ISubscribe>list;
 };
 class ViewObserver:public Observer {
 public:
 	void subscribe(ISubscribe& subscriber) {}
 	void publish(string payload) {}
 };
-class View:public ViewObserver {
+class View:public ViewObserver,public ISubscribe {
 public:
-	
 	virtual void on_input() = 0;
+	virtual void on_click() = 0;
 
 	string value;
 };
-
-int main() {
+class Input:public View {
+public:
+	void on_input(){}
+};
+class TextArea:public Input {
+public:
+	void on_input() {}
+};
+class Urilnput :public Input {
+public:
+	void on_input() {}
+};
+class Btn :public View {
+public:
+	void on_click() {}
+};
+class SaveBtn:public Btn {
+public:
+	void on_click() {}
+};
+class OpenBtn :public Btn {
+public:
+	void on_click() {}
+};
+int main(){
 
 	return 0;
 }
